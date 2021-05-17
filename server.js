9e8f4368f92aa11053ebd7cd3f666ecfc1fc6196
Aaron Reason <aaronreason@hotmail.co.uk>
2021-05-17 10:03:06 +0100

    initial setup

---
+++ server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const path = require("path")
+
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose")
+
+// mongoose.connect(process.env.DBURL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// const db = mongoose.connection;
+// db.on("error", (error) => console.error(error));
+// db.once("open", () => console.log("connected to db"))
+
+app.use(express.json());
+
+
+app.get("/", (req, res) =>{
+    res.sendFile(path.join(__dirname, "index.html"))
+})
+
+app.listen(3000, () => console.log("server started"));
