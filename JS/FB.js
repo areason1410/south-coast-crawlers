9581105f2081891a1510e5c51908b8c76643f205
Balamurali-Gif <71128841+Balamurali-Gif@users.noreply.github.com>
2021-05-20 13:58:55 +0100

    Added Login Page

--- JS/FB.js
+++
@@ -1,7 +0,0 @@
-var finished_rendering = function() {
-    console.log("finished rendering plugins");
-    var spinner = document.getElementById("spinner");
-    spinner.removeAttribute("style");
-    spinner.removeChild(spinner.childNodes[0]);
-  }
-  FB.Event.subscribe('xfbml.render', finished_rendering);
