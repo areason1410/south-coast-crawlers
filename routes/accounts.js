8418e35a9f62dd88c834d05a6a29cb7bb316960c
Aaron Reason <aaronreason@hotmail.co.uk>
2021-05-25 13:48:20 +0100

    fixed

--- routes/accounts.js
+++ routes/accounts.js
@@ -21,111 +21,97 @@ router.get("/", async (req, res) => {
 router.get("/:id", getAccount, (req,res) => {
     res.send(res.account);
 
-    router.get("/:email/:password", checkPassword, async (req,res) => {
-        res.send({res: res.passwordCheck});
-     })
+})
 
-    //create an account
-    router.post("/", async (req,res) => {
-        const hashed = hash.encrypt(req.body.password)
-        const account = new Account({
-            username: req.body.username,
-            profilePicture: req.body.profilePicture,
-            email: req.body.email,
-            password: hashed.encryptedData,
-            isAdmin: req.body.isAdmin,
-            creationDate: req.body.creationDate,
-            iv: hashed.iv
-        })
+router.get("/:email/:password", checkPassword, async (req,res) => {
+    res.send({res: res.passwordCheck});
+ })
 
-        try{
-            const newAccount = await account.save();
-            res.status(201).json({res: "Created Account", account: newAccount});
-        }catch(err){
-            res.status(400).json({res:err})
-        }
+//create an account
+router.post("/", async (req,res) => {
+    const hashed = hash.encrypt(req.body.password)
+    const account = new Account({
+        username: req.body.username,
+        profilePicture: req.body.profilePicture,
+        email: req.body.email,
+        password: hashed.encryptedData,
+        isAdmin: req.body.isAdmin,
+        creationDate: req.body.creationDate,
+        iv: hashed.iv
     })
 
     try{
         const newAccount = await account.save();
-        res.status(201).json({message: "Created Account", account: newAccount});
+        res.status(201).json({res: "Created Account", account: newAccount});
     }catch(err){
-        res.status(400).json({message:err})
+        res.status(400).json({res:err})
     }
 })
 
-    //delete an account
-    router.delete("/:id", getAccount, async (req,res) => {
-        try{
-            await res.account.remove();
-            res.json({res: "Deleted Account", account: res.account})
-        }catch(err){
-            res.status(500).json({res:err})
-        }
-    })
+
 
 //edit an account
 router.patch("/:id", getAccount, async (req, res) => {
     if (req.body.name != null) {
-      res.account.name = req.body = req.body.name;
+        res.account.name = req.body = req.body.name;
     }
     if (req.body.name != null) {
-      res.account.name = req.body = req.body.name;
+        res.account.name = req.body = req.body.name;
     }
-  
+    
     try {
-      const updatedAccount = await res.account.save();
-      res.json(updatedAccount);
+        const updatedAccount = await res.account.save();
+        res.json(updatedAccount);
     } catch (err) {
-      res.status(400).json({ message: err.message });
+        res.status(400).json({ message: err.message });
     }
-  });
+    });
 
 //delete an account
 router.delete("/:id", getAccount, async (req,res) => {
     try{
         await res.account.remove();
-        res.json({message: "Deleted Account", account: res.account})
+        res.json({res: "Deleted Account", account: res.account})
     }catch(err){
-        res.status(500).json({message:err})
+        res.status(500).json({res:err})
     }
 })
 
 async function getAccount(req, res, next){
 
-        let account;
-        try{
-            account = await Account.findById(req.params.id);
-            if(account == null){
-                return res.status(404).json({res: "Cannot find that account"});
-            }
-        }catch(err){
-            return res.status(500).json({res: err.res})
+    let account;
+    try{
+        account = await Account.findById(req.params.id);
+        if(account == null){
+            return res.status(404).json({res: "Cannot find that account"});
         }
+    }catch(err){
+        return res.status(500).json({res: err.res})
+    }
     res.account = account;
     next();
 }
 
-    async function checkPassword(req, res, next){
-        var passwordCheck = false;
-        try{
-            const account = await Account.findOne({"email": req.params.email});
-            if(account == null){
-                return res.status(404).json({res: "Cannot find that account"});
-            }
-            const decryptedPassword = hash.decrypt({"encryptedData": account.password,
-            "iv": account.iv})
-
-            if(decryptedPassword == req.params.password)
-            {
-                passwordCheck = true;
-            }
-        } catch(err) {
-            return res.status(500).json({res: err.message})
+async function checkPassword(req, res, next){
+    var passwordCheck = false;
+    try{
+        const account = await Account.findOne({"email": req.params.email});
+        if(account == null){
+            return res.status(404).json({res: "Cannot find that account"});
         }
-        res.passwordCheck = passwordCheck;
-        next();
+        const decryptedPassword = hash.decrypt({"encryptedData": account.password,
+        "iv": account.iv})
+
+        if(decryptedPassword == req.params.password)
+        {
+            passwordCheck = true;
+        }
+    } catch(err) {
+        return res.status(500).json({res: err.message})
     }
+    res.passwordCheck = passwordCheck;
+    next();
+}
 //--------------------------- requests ---------------------------//
 
 
