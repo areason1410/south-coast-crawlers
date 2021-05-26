475f3779ab27d06f6929ab8e5cec9f4dc6bf7e6e
Aaron Reason <aaronreason@hotmail.co.uk>
2021-05-26 21:54:42 +0100

    quickly sorted the date and display username as name is redacted also fixed admin upgrade button thing

--- public/resources/js/profileScript.js
+++ public/resources/js/profileScript.js
@@ -1,15 +1,18 @@
 databaseRequest("http://localhost:3000/south-coast-crawlers/accounts/60a7bc09a4f3c1d60f58d1f9", "GET")
 .then(data => {
-    console.log(data)
     document.getElementById('profilePic').src = data.profilePicture
-    document.getElementById('profileName').innerHTML = "Name: " + data.name 
+    document.getElementById('profileName').innerHTML = "username: " + data.username 
     if(data.isAdmin == false) {
         document.getElementById('profileStatus').innerHTML = "Account Status: User"
-        document.getElementById('button').style.display = "none"
     } else if (data.isAdmin == true) {
         document.getElementById('profileStatus').innerHTML = "Account Status: Admin"
+        document.getElementById('button').style.display = "none"
     } 
     document.getElementById('profileEmail').innerHTML = "email: " + data.email
-    document.getElementById('profileDate').innerHTML = "Joined: " + data.creationDate  
+    var creationDate = data.creationDate;
+    var creationDateTemp = creationDate.indexOf("T");
+    creationDate = creationDate.slice(0, creationDateTemp);
+    creationDate = `${creationDate.slice(8, 10)}/${creationDate.slice(5, 7)}/${creationDate.slice(0,4)}`
+    document.getElementById('profileDate').innerHTML = "Joined: " + creationDate;
 })
 
