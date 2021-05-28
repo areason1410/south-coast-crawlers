b6b1c551c97914ddc59cc495df147a106dd5886c
BMajor65 <benmajor6565@yahoo.com>
2021-05-28 11:29:09 +0100

    add javascript for buttons

---
+++ public/resources/js/adminPageButtons.js
@@ -0,0 +1,24 @@
+var button = document.querySelector('.eventSetting')
+
+button.addEventListener('click', function() {
+
+})
+
+var button = document.querySelector('.endRound')
+
+button.addEventListener('click', function() {
+
+})
+
+var button = document.querySelector('.endEvent')
+
+button.addEventListener('click', function() {
+    addElements() 
+})
+
+function addElements() {
+    var h1 = document.createElement('h1')
+    h1.classList.add('header')
+    h1.innerHTML
+}
+
