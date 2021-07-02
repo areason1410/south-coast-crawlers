465816cd204da204627c2efa54c8ef062352118e
HSanderson <hay.saw13@gmx.co.uk>
2021-07-02 11:12:07 +0100

    html-improvements

---
+++ public/resources/js/leaderboard.js
@@ -0,0 +1,3 @@
+fetch["http://localhost:3000/south-coast-crawlers/events/60acf26b00f321e4e88ec902/60ab6bac861e918a606f942f"]
+.then[response => response.jsom()]
+.then[data => document.querySelector".test".innerHTML = data]
