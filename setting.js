c92d396f474b467650fd48d87e5b4e5e3e5ec9a7
A. hamilton <ahamilton@salisbury6c.ac.uk>
2021-07-06 13:26:26 +0100

    added settings

---
+++ setting.js
@@ -0,0 +1,15 @@
+
+var buttons = document.querySelectorAll(".button") // button variable 
+
+var i;
+
+for (i = 0; i < buttons.length;)
+
+    console.log(buttons)
+
+addEventListener("click", function () {
+    const element1 = document.getElementById("1")
+    element1.innerHTML = "pick profile picture"
+    element1.innerHTML 
+    return 0
+})
